423de68ea04b27dd916b4189796238247a790d08
ajmal <ajmalpn.1007@gmail.com>
2024-07-27 22:25:29 +0530

    small changes to SVM

--- svm.py
+++ svm.py
@@ -5,6 +5,7 @@ class SVM:
     # Initializing the instance of the class
     def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
         self.lr = learning_rate
+        # Regularization parameter to prevent overfitting
         self.lambda_param = lambda_param
         self.n_iters = n_iters
         self.w = None
@@ -14,13 +15,16 @@ class SVM:
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
+        # y_ is y converted to -1 or 1
         y_ = np.where(y <= 0, -1, 1)
 
         self.w = np.zeros(n_features)
         self.b = 0
 
         for _ in range(self.n_iters):
+            # For each sample x_i in X
             for idx, x_i in enumerate(X):
+                # Check the condition - If the sample is correctly classified or not
                 condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
                 if condition:
                     self.w -= self.lr * (2 * self.lambda_param * self.w)
